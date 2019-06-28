9d35a6caf1120daa651a8b7e18cb1ae3ac34ec79
Herman Gouw <hermangouw@gmail.com>
2019-06-28 21:04:42 +1000

    Added MatAutoCompleteComponent

---
+++ src/app/mat-auto-complete/mat-auto-complete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mat-auto-complete',
+  templateUrl: './mat-auto-complete.component.html',
+  styleUrls: ['./mat-auto-complete.component.css']
+})
+export class MatAutoCompleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
